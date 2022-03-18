327e111a5b9ee46188ec208c12dfdbec67d84383
mohamedisakr <mohamedisakr@gmail.com>
2022-03-18 08:48:34 +0200

    adding bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
